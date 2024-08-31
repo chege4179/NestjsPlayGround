db2d8cdae9c83160f28f4859565a3c93669ace1c
Peter Chege <peterkagure@gmail.com>
2024-09-01 00:07:21 +0300

    Some cleanups

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,8 @@
-import { Module } from "@nestjs/common";
-import { AuthModule } from "./auth/auth.module";
-import { UserModule } from "./user/user.module";
-import { BookmarkModule } from "./bookmark/bookmark.module";
-import { PrismaModule } from "./prisma/prisma.module";
-import { ConfigModule } from "@nestjs/config";
+import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
+import { ConfigModule } from '@nestjs/config';
 
 
 @Module({
@@ -11,7 +10,6 @@ import { ConfigModule } from "@nestjs/config";
           AuthModule,
           UserModule,
           BookmarkModule,
-          PrismaModule,
           ConfigModule.forRoot({
                isGlobal:true
           })
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,7 @@
-import { Body, Controller, ParseIntPipe, Post, Req } from "@nestjs/common";
-import { AuthService } from "./auth.service";
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
-import { AuthDto } from "./dto";
+import { AuthDto } from './dto';
 
 
 @Controller("auth")
